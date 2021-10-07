ca10f552a64f4d555977302fed70633e4f70b36f
wslmacieira <wslmacieira@outlook.com>
2021-10-07 10:08:53 -0300

    Generate Components in Angular

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,11 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductByDateComponent } from './view-product-by-date/view-product-by-date.component';
+import { ViewProductByCategoryComponent } from './view-product-by-category/view-product-by-category.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { ViewAllProductComponent } from './view-all-product/view-all-product.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    AddProductComponent,
+    ViewProductByDateComponent,
+    ViewProductByCategoryComponent,
+    ViewProductComponent,
+    UpdateProductComponent,
+    DeleteProductComponent,
+    ViewAllProductComponent
   ],
   imports: [
     CommonModule,
